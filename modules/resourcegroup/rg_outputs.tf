e1860bfcca176653fab8922534b1ebdad3d3cc4f
Divya-mandati <123730077+DivyaMandati@users.noreply.github.com>
2023-10-23 20:11:51 +0100

    Delete modules directory

--- modules/resourcegroup/rg_outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "The id of the resource group."
-  value       = azurerm_resource_group.resource_group.id
-}
-
-output "name" {
-  description = "The name of the resource group."
-  value       = azurerm_resource_group.resource_group.name
-}
